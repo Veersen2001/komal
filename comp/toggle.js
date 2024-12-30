8a8bfeab46ef909223e4806fc726c1fb9a2b5683
Veerbahadur Sen <veerbahadursen@gmail.com>
2024-12-30 17:08:58 +0530

    Delete comp directory

--- comp/toggle.js
+++
@@ -1,7 +0,0 @@
-
-    const toggleBtn = document.getElementById('toggle-btn');
-    const navMenu = document.getElementById('nav-menu');
-    toggleBtn.addEventListener('click', () => {
-      navMenu.classList.toggle('active');
-    });
-  
