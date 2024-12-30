9d467cb1f0f328ac54cf49ae3cd59b689ca3be0a
Veerbahadur Sen <veerbahadursen@gmail.com>
2024-12-30 17:01:41 +0530

    Add files via upload

---
+++ comp/toggle.js
@@ -0,0 +1,7 @@
+
+    const toggleBtn = document.getElementById('toggle-btn');
+    const navMenu = document.getElementById('nav-menu');
+    toggleBtn.addEventListener('click', () => {
+      navMenu.classList.toggle('active');
+    });
+  
